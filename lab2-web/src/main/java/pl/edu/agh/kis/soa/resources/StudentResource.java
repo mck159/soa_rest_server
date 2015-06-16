27c1373d58c0f983b7f85b1b86266ca4463d4c20
Maciej Sroka <mck159@gmail.com>
2015-06-16 19:45:05 +0200

    REST

---
+++ lab2-web/src/main/java/pl/edu/agh/kis/soa/resources/StudentResource.java
@@ -0,0 +1,68 @@
+package pl.edu.agh.kis.soa.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import pl.edu.agh.kis.soa.resources.model.Student;
+
+/**
+ * Klasa wystawiająca interfejs REST.
+ *
+ * @author teacher
+ */
+@Path("rest")
+@Stateless
+public class StudentResource {
+
+    private static final Logger logger = Logger.getLogger("StudentResource");
+
+    public StudentResource() {
+    }
+
+    @GET
+    @Path("student/{albumNo}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Student readStudent(@PathParam("albumNo") String albumNo) {
+        if (albumNo.equals("258433")) {
+            Student s = new Student();
+            s.setFirstName("Maciek");
+            s.setLastName("Sroka");
+            s.setAlbumNo("258433");
+            s.setSubjects(Arrays.asList("SOA", "Bazy danych"));
+            return s;
+        }
+        return null;
+    }
+
+    @POST
+    @Path("student")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createStudent(Student student) {
+        String result = String.format("Location: student/%s", student.getAlbumNo());
+        return Response.status(Response.Status.CREATED).entity(result).build();
+    }
+
+    @PUT
+    @Path("student/{albumNo}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateStudent(Student student) {
+        String result = String.format("Location: student/%s", student.getAlbumNo());
+            return Response.status(Response.Status.OK).entity(result).build();
+    }
+
+    @DELETE
+    @Path("student/{albumNo}")
+    public Response deleteStudent(@PathParam("albumNo") String albumNo) {
+        if(albumNo.equals("258433")) {
+            return Response.status(Response.Status.OK).build();
+        }
+        return null;
+    }
+}
